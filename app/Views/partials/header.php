2e0e67d7da31a62325cc4080fce7352c10e21c63
woody180 <woody180@rocketmail.com>
2024-07-27 11:10:12 +0400

    update ui

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -14,7 +14,7 @@
 
         <div class="nav-overlay uk-navbar-right">
 
-            <a class="uk-button uk-button-default uk-button-small uk-border-rounded" href="<?= baseUrl('words/add') ?>">
+            <a class="uk-button uk-button-default add-word-nav uk-button-small uk-border-rounded" href="<?= baseUrl('words/add') ?>">
                 <span uk-icon="icon: plus; ratio: .6"></span>
                 <?= translate('translations.add_word') ?>
             </a>
