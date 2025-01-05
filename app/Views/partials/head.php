0055c9065f88d42189b01d940efba4c8afee5f32
woody180 <woody180@rocketmail.com>
2025-01-05 11:48:11 +0400

    favicon added

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -14,6 +14,9 @@
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.24/dist/js/uikit.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.24/dist/js/uikit-icons.min.js"></script>
 
+    <link rel="icon" type="image/png" href="<?= assetsUrl('favicon.png') ?>">
+
+
     
     <title><?= $title ?? APPNAME; ?></title>
 </head>
