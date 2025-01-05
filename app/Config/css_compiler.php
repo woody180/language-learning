768155dcdd8ad485bd35880130923b5dfac29eed
woody180 <woody180@rocketmail.com>
2025-01-05 11:34:44 +0400

    turn off scss compiler

--- app/Config/css_compiler.php
+++ app/Config/css_compiler.php
@@ -1,6 +1,6 @@
 <?php
 
-define("COMPILE", TRUE);
+define("COMPILE", FALSE);
 define("MINIFY", TRUE);
 define("COMPILATIONS_FILE_FROM", 'bootstrap.scss');
 define("COMPILED_FILE_TO", 'main.min.css');
